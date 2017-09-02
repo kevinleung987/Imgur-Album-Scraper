90c1e4c5e5055fb70a21cd1b71e976a5508650b3
kevinleung987 <kevin.leung987@hotmail.com>
2017-09-02 14:42:07 -0400

    Skip duplicates, download from links
    Skip downloads for existing images, can be run to take in links and download the images.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Imgur Album Scraper
-Python Imgur Scraper that scrapes all the images of an Imgur album into a folder.
+Python Imgur Scraper that scrapes all the images of an Imgur album into a folder. If you have already downloaded the album in the past, duplicate images won't be overwritten and only new additions to the album will be downloaded. Should work with any kind of file hosted inside of an Imgur album.
+# Instructions/How to Use
+- Install Python 3
+- Install the Requests library by running the following command.
+```sh
+$ python pip -m install requests
+```
+- Replace client_id with your own Imgur API Application Client ID, you can acquire one in this page https://api.imgur.com/oauth2/addclient
+- Modify directory_name to what you want, the directory will be created and the album folders will be generated within that directory. An empty string will generate album folders in the same directory as the Python file.
+- Place main.py where you want the album directories to be stored and run it.
+- Input the Imgur album links that you want to download.
+
+License
+----
+MIT
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import requests
 import os
 import shutil
 
-client_id = ''  # Imgur application Client ID, fill this in.
-directory_name = 'Imgur Albums\\'  # Directory for the images to be stored
+client_id = 'e36ce095653e924'  # Imgur application Client ID, fill this in
+# Change directory_name into an empty string for images to be stored in the
+# same directory as the python file
+directory_name = 'Imgur Albums\\'
 
 
 def scrape(album_id):
@@ -13,10 +15,9 @@ def scrape(album_id):
     '''
     # Seperate directory for each album's images to be stored in
     directory = directory_name+album_id+'\\'
-    if not os.path.exists(directory):
-        os.makedirs(directory)  # Creates the full directory for the album
+    scraped = []
     imageList = []
-    print('Loading Album: '+album_id)
+    print('Downloading Album: '+album_id)
     # Loading the album with Requests
     link = 'https://api.imgur.com/3/album/' + album_id
     header = {'Authorization': 'Client-Id '+client_id}
@@ -26,9 +27,27 @@ def scrape(album_id):
     # Scrape image links from the album
     for image in album['data']['images']:
         imageList.append(image['link'])
+    # Creates the full directory for the album
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     # Downloads each image and writes to disk
     for image in imageList:
         download = requests.get(image, stream=True)
-        with open(directory+image[image.find('com/')+4:], 'wb') as outFile:
-            shutil.copyfileobj(download.raw, outFile)
-    return imageList
+        imagepath = directory+image[image.find('com/')+4:]
+        # Only download the image if it does not already exist in the folder
+        if not os.path.isfile(imagepath):
+            print('Downloaded '+imagepath)
+            with open(imagepath, 'wb') as outFile:
+                shutil.copyfileobj(download.raw, outFile)
+            scraped.append(image)
+    return scraped
+
+if __name__ == '__main__':
+    while True:
+        link = input('Input a Imgur album link: ')
+        if '/a/' in link:
+            scrape(link[link.find('/a/')+3:])
+        elif '/gallery/' in link:
+            scrape(link[link.find('/gallery/')+9:])
+        else:
+            print('Invalid link! Example: https://imgur.com/a/cusMo')
